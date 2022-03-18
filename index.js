66e23755a276e2eb61a01d151e5e92a9ee93b256
Matheus Rhazek <matheus_rhazek@hotmail.com>
2022-03-18 19:30:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+  this.style.color = "white";
+
+});
+
+}
+
+
+
+// let tom1 = new Audio('sounds/tom-1.mp3');
+// tom1.play();
